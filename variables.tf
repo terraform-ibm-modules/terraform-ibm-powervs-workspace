ab47063bcc3e06487dfa9683b28e80c9814115eb
surajsbharadwaj <suraj.bharadwaj@ibm.com>
2024-01-09 15:50:47 +0100

    feat: support wdc07, mad04, sao04 eu-de-2 per enabled DC

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "pi_zone" {
   type        = string
   validation {
     condition     = contains(["syd04", "syd05", "eu-de-1", "eu-de-2", "lon04", "lon06", "us-east", "wdc06", "wdc07", "us-south", "dal10", "dal12", "tok04", "osa21", "sao01", "sao04", "mon01", "tor01", "mad02", "mad04"], var.pi_zone)
-    error_message = "Only Following DC values are supported : syd04, syd05, eu-de-1, eu-de-2, lon04, lon06, us-east, wdc06, us-south, dal10, dal12, tok04, osa21, sao01, sao04 mon01, tor01, mad02, mad04"
+    error_message = "Only Following DC values are supported : syd04, syd05, eu-de-1, eu-de-2, lon04, lon06, us-east, wdc06, wdc07, us-south, dal10, dal12, tok04, osa21, sao01, sao04 mon01, tor01, mad02, mad04"
   }
 }
 
