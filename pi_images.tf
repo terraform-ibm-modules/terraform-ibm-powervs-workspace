04e03e777a23ace21258780a865cd259bc2dca68
surajsbharadwaj <101711050+surajsbharadwaj@users.noreply.github.com>
2025-02-18 11:45:37 +0100

    feat: support vtl image data source (#214)

--- pi_images.tf
+++ pi_images.tf
@@ -5,6 +5,7 @@
 data "ibm_pi_catalog_images" "catalog_images_ds" {
   pi_cloud_instance_id = ibm_resource_instance.pi_workspace.guid
   sap                  = true
+  vtl                  = true
 }
 
 locals {
@@ -18,7 +19,6 @@ resource "ibm_pi_image" "import_images" {
   for_each             = toset(var.pi_image_names)
   pi_cloud_instance_id = ibm_resource_instance.pi_workspace.guid
   pi_image_id          = lookup(local.catalog_images, each.key, null)
-  pi_image_name        = each.key
   pi_user_tags         = var.pi_tags != null ? var.pi_tags : []
 
   timeouts {
