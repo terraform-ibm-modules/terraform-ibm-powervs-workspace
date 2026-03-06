8fc7d9497e19f0ae132d89cfc405dd33ccbf2170
BabithaPadiri <babitha.padiri@ibm.com>
2026-03-06 17:02:44 +0530

    feat : added support to chennai DC

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "pi_zone" {
   type        = string
   validation {
     condition     = contains(["syd04", "syd05", "eu-de-1", "eu-de-2", "lon04", "lon06", "us-east", "wdc06", "wdc07", "us-south", "dal10", "dal12", "tok04", "osa21", "sao01", "sao04", "sao05", "mon01", "tor01", "mad02", "mad04", "dal14", "che01", "che02", "che03"], var.pi_zone)
-    error_message = "Only Following DC values are supported : syd04, syd05, eu-de-1, eu-de-2, lon04, lon06, us-east, wdc06, wdc07, us-south, dal10, dal12, tok04, osa21, sao01, sao04, sao05, mon01, tor01, mad02, mad04, dal14, che02, che03,che01"
+    error_message = "Only Following DC values are supported : syd04, syd05, eu-de-1, eu-de-2, lon04, lon06, us-east, wdc06, wdc07, us-south, dal10, dal12, tok04, osa21, sao01, sao04, sao05, mon01, tor01, mad02, mad04, dal14, che02, che03, che01"
   }
 }
 
